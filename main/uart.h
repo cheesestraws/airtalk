00fe0defb8c4d636fe91a8b2edcb3a0ca11dc58c
Rob Mitchelmore <rob@midworld.co.uk>
2022-01-02 17:20:19 +0000

    move tx wedge detection

--- main/uart.h
+++ main/uart.h
@@ -2,9 +2,17 @@
 #define UART_H
 
 #include "buffer_pool.h"
+#include "node_table.h"
 
 void uart_init(void);
 void uart_init_tashtalk(void);
 void uart_start(buffer_pool_t* packet_pool, QueueHandle_t txQueue, QueueHandle_t rxQueue);
+void uart_check_for_tx_wedge(llap_packet* packet);
+
+// get_proxy_nodes returns the node_table for the nodes that we are proxying for
+// on the UART side, which is the set of nodes we have seen on the *Wifi*
+// side.
+node_table_t* get_proxy_nodes();
+
 
 #endif
