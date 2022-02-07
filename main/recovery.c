075c58378c0fff82c3c7adb6e935aa1ceae39502
Rob Mitchelmore <rob@midworld.co.uk>
2022-02-07 15:41:26 +0000

    make recovery page do something

--- main/recovery.c
+++ main/recovery.c
@@ -1,4 +1,7 @@
 #include <string.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <ctype.h>
 
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
@@ -10,7 +13,6 @@
 #include "esp_log.h"
 #include "esp_http_server.h"
 #include "nvs_flash.h"
-
 #include "lwip/err.h"
 #include "lwip/sys.h"
 
@@ -21,6 +23,45 @@ const static char* TAG = "recovery-app";
 
 static esp_netif_t *ap_if;
 static httpd_handle_t srv;
+
+#include <stdlib.h>
+#include <ctype.h>
+
+void urlndecode(char *dst, const char *src, size_t len)
+{
+        char a, b;
+        size_t cnt = 0;
+        while (*src) {
+                if ((*src == '%') &&
+                    ((a = src[1]) && (b = src[2])) &&
+                    (isxdigit(a) && isxdigit(b))) {
+                        if (a >= 'a')
+                                a -= 'a'-'A';
+                        if (a >= 'A')
+                                a -= ('A' - 10);
+                        else
+                                a -= '0';
+                        if (b >= 'a')
+                                b -= 'a'-'A';
+                        if (b >= 'A')
+                                b -= ('A' - 10);
+                        else
+                                b -= '0';
+                        *dst++ = 16*a+b;
+                        src+=3;
+                } else if (*src == '+') {
+                        *dst++ = ' ';
+                        src++;
+                } else {
+                        *dst++ = *src++;
+                }
+                cnt++;
+                if (cnt >= (len - 1)) {
+                	break;
+                }
+        }
+        *dst++ = '\0';
+}
 
 void init_recovery_wifi(void) {
 	char myssid[33] = {0};
@@ -62,17 +103,43 @@ void init_recovery_wifi(void) {
 
 static esp_err_t root_handler(httpd_req_t *req) {
 	char* qry;
+	// urlencoded ssid and key might be up to 3x the length of 'real'
+	// ssid and key if every character is %-encoded.  (Note the last char
+	// of each is a null, so we don't actually have 3* number of characters)
+	bool got_ssid;
+	char ssid[33] = {0};
+	char encoded_ssid[97] = {0};
+	bool got_key;
+	char key[65] = {0};
+	char encoded_key[193] = {0};
 
 	// Do we have a query string?
 	size_t buf_len = httpd_req_get_url_query_len(req) + 1;
 	if (buf_len > 1) {
 		qry = malloc(buf_len);
-		
-		char ssid[33] = {0};
-		char key[65] = {0};
+		if (httpd_req_get_url_query_str(req, qry, buf_len) == ESP_OK) {		
+			if (httpd_query_key_value(qry, "ssid", encoded_ssid, sizeof(encoded_ssid)) == ESP_OK) {
+				urlndecode(ssid, encoded_ssid, sizeof(ssid));
+				got_ssid = true;
+				ESP_LOGI(TAG, "setting ssid => %s", ssid);
+			}
+			
+			if (httpd_query_key_value(qry, "key", encoded_key, sizeof(encoded_key)) == ESP_OK) {
+				urlndecode(key, encoded_key, sizeof(key));
+				got_key = true;
+				ESP_LOGI(TAG, "setting key => %s", key);
+			}
+		}
+		free(qry);
 	}
 
-	httpd_resp_send(req, details_page, HTTPD_RESP_USE_STRLEN);
+	if (got_ssid && got_key) {
+		httpd_resp_send(req, ok_page, HTTPD_RESP_USE_STRLEN);
+		ESP_LOGI(TAG, "would save (%s, %s) and reboot here", ssid, key);
+		
+	} else {
+		httpd_resp_send(req, details_page, HTTPD_RESP_USE_STRLEN);
+	}
 	return ESP_OK;
 }
 
